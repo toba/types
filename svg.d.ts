69ec0440757bf1d36379cffdc1b357a94b1dc06d
Jason Abbott <jasonabbott@Jason-Abbot-MBP15tb.local>
2019-02-27 14:21:36 -0700

    doc

--- svg.d.ts
+++ svg.d.ts
@@ -1,5 +1,8 @@
 declare module '*.svg' {
    import React from 'react';
+   /**
+    * @see https://github.com/Microsoft/TypeScript/issues/2709
+    */
    const svg: React.ReactSVGElement;
    export default svg;
 }
